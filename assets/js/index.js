05459c8efa2b437151dfc0438b585c4731282dc9
kalugot <krsudeep24@Rams-MacBook-Pro.local>
2020-03-30 23:29:43 -0400

    Changes with new experience and resume

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,10 +12,6 @@ $(document).ready(function () {
 
 
 jQuery(document).ready(function ($) {
-    // Scroll to the desired section on click
-    // Make sure to add the `data-scroll` attribute to your `<a>` tag.
-    // Example: 
-    // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
     function scrollToSection(event) {
         event.preventDefault();
         var $section = $($(this).attr('href'));
